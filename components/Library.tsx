d18d8b1b069b65327a99d6d89d0d92a654714d1a
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-17 12:12:41 -0400

    Created Libary Component
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ components/Library.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { TbPlaylist } from 'react-icons/tb';
+import { AiOutlinePlus } from 'react-icons/ai';
+
+const Library = () => {
+  const onClick = () => {};
+
+  return (
+    <div className='flex flex-col'>
+      <div className='flex items-center justify-between px-5 pt-4'>
+        <div className='inline-flex items-center gap-x-2'>
+          <TbPlaylist size={26} className='text-neutral-400' />
+          <p className='font-medium text-md text-neutral-400'>Your Library</p>
+        </div>
+        <AiOutlinePlus
+          className='cursor-pointer text-neutral-400 hover:text-white transition'
+          onClick={onClick}
+          size={20}
+        />
+      </div>
+      <div className='flex flex-col gap-y-2 mt-4 px-3'>Songs!</div>
+    </div>
+  );
+};
+
+export default Library;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { BiSearch } from 'react-icons/bi';
 
 import Box from './Box';
 import SidebarItem from './SidebarItem';
+import Library from './Library';
 
 type AppProps = {
   children: React.ReactNode;
@@ -43,8 +44,11 @@ const Sidebar = ({ children }: AppProps) => {
             ))}
           </div>
         </Box>
-        <Box className='overflow-y-auto h-full'>Song Library</Box>
+        <Box className='overflow-y-auto h-full'>
+          <Library />
+        </Box>
       </div>
+      <main className='h-full flex-1 overflow-y-auto py-2'>{children}</main>
     </div>
   );
 };
