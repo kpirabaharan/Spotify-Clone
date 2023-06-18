33908939fd7b52246155891fcfaed384f97c654d
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-17 20:57:33 -0400

    Created Supabase Provider Wrapper Component
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Figtree } from 'next/font/google';
 
+import SupabaseProvider from '@/providers/SupabaseProvider';
 import Sidebar from '@/components/Sidebar';
 
 const font = Figtree({ subsets: ['latin'] });
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   );
--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,9 +1,23 @@
 'use client';
 
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
 import { Database } from '@/types_db';
 
 interface SupabaseProviderProps extends PropsWithChildren {}
 
-const SupabaseProvider = ({ children }: SupabaseProviderProps) => {};
+const SupabaseProvider = ({ children }: SupabaseProviderProps) => {
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>(),
+  );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
+
+export default SupabaseProvider;
