3c4d36e525fc49ea540e4d46ad968b4021f2c8f1
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-23 16:20:00 -0400

    Search Functionality
    Neet Trick to Add Debounce
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Song } from '@/types';
+import MediaItem from '@/components/MediaItem';
+
+interface SearchContentProps {
+  songs: Song[];
+}
+
+const SearchContent = ({ songs }: SearchContentProps) => {
+  if (songs.length === 0) {
+    return (
+      <div className='flex flex-col gap-y-2 w-full px-6 text-neutral-400'>
+        No Songs Found
+      </div>
+    );
+  }
+
+  return (
+    <div className='flex flex-col gap-y-2 w-full px-6 '>
+      {songs.map((song) => (
+        <div key={song.id} className='flex items-center gap-x-4 w-full'>
+          <div className='flex-1 '>
+            <MediaItem data={song} onClick={() => {}} />
+          </div>
+
+          {/* //TODO: Add Like Button*/}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SearchContent;
---
+++ components/SearchInput.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import qs from 'query-string';
+
+import useDebounce from '@/hooks/useDebounce';
+
+import Input from './Input';
+
+const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState('');
+  const debouncedValue = useDebounce(value, 500);
+
+  useEffect(() => {
+    const query = { title: debouncedValue };
+
+    const url = qs.stringifyUrl({ url: '/search', query: query });
+
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <Input
+      placeholder='Search for a song'
+      value={value}
+      onChange={(val) => setValue(val.target.value)}
+    />
+  );
+};
+
+export default SearchInput;
