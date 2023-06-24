55259c12c612c60c6d3ba83212398d01bbed1be9
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-24 17:47:01 -0400

    Created Player at Bottom of Screen
    Only shows when song is liked play
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ components/Player.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import usePlayer from '@/hooks/usePlayer';
+import useLoadSong from '@/hooks/useLoadSong';
+import useGetSongById from '@/hooks/useGetSongById';
+
+import PlayerContent from './PlayerContent';
+
+const Player = () => {
+  const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
+
+  const songUrl = useLoadSong(song!);
+
+  if (!song || !songUrl || !player.activeId) {
+    return null;
+  }
+
+  return (
+    <div className='fixed bottom-0 bg-black w-full py-2 h-[80px] px-4'>
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
+    </div>
+  );
+};
+
+export default Player;
