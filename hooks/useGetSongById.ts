a7223b5b7902dbddf6ed7e02272cedd9028170be
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-24 16:59:35 -0400

    Hook to Get Song to Client Side Component
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ hooks/useGetSongById.ts
@@ -0,0 +1,47 @@
+import { useState, useEffect, useMemo } from 'react';
+
+import { Song } from '@/types';
+import { useSessionContext } from '@supabase/auth-helpers-react';
+import { toast } from 'react-hot-toast';
+
+const useGetSongById = (id?: string) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [song, setSong] = useState<Song | undefined>(undefined);
+  const { supabaseClient } = useSessionContext();
+
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const fetchSong = async () => {
+      const { data, error } = await supabaseClient
+        .from('songs')
+        .select('*')
+        .eq('id', id)
+        .single();
+
+      if (error) {
+        setIsLoading(false);
+        return toast.error(error.message);
+      }
+
+      setSong(data as Song);
+      setIsLoading(false);
+    };
+
+    fetchSong();
+  }, [id, supabaseClient]);
+
+  return useMemo(
+    () => ({
+      isLoading,
+      song,
+    }),
+    [isLoading, song],
+  );
+};
+
+export default useGetSongById;
