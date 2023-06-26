7cddc186ef528d21bb89b72078b6953c55171f9a
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-26 13:38:52 -0400

    Created Stripe Billing Portal API
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ app/api/create-portal-link/route.ts
@@ -0,0 +1,40 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+import { stripe } from '@/libs/stripe';
+import { getUrl } from '@/libs/helpers';
+import { createOrRetrieveCustomer } from '@/libs/supabaseAdmin';
+
+export const POST = async () => {
+  try {
+    const supabase = createRouteHandlerClient({ cookies });
+
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+      throw new Error('Could not get user');
+    }
+
+    const customer = await createOrRetrieveCustomer({
+      uuid: user.id || '',
+      email: user.email || '',
+    });
+
+    if (!customer) {
+      throw new Error('Could not get customer');
+    }
+
+    const { url } = await stripe.billingPortal.sessions.create({
+      customer,
+      return_url: `${getUrl()}/account`,
+    });
+
+    return NextResponse.json({ url });
+  } catch (err: any) {
+    console.log(err);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+};
