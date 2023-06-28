c104c619ef2bd433ecdc1539b6aa067c8026f991
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-27 23:09:08 -0400

    Project Completed
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/Player.tsx
+++ components/Player.tsx
@@ -16,8 +16,6 @@ const Player = () => {
     return null;
   }
 
-  console.log('Player');
-
   return (
     <div className='fixed bottom-0 bg-black w-full py-2 h-[80px] px-4'>
       <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
