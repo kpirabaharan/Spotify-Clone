2baa01c9c2cb89e49453b385c55d888a396345cd
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-28 21:18:14 -0400

    Re-added Github Auth with Fix
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -41,7 +41,7 @@ const AuthModal = () => {
     >
       <Auth
         theme='dark'
-        providers={[]}
+        providers={['github']}
         magicLink
         supabaseClient={supabaseClient}
         appearance={{
